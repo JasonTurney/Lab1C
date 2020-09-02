5167774fcd685051f379105462c326991acf712d
Jason Turney <jnt5211@psu.edu>
2020-09-02 19:54:58 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <readline/readline.h>
+
+int main(void) {
+  char *celsius = readline("Enter temperature in celsius: ");
+  atof(celsius)
+  fahrenheit = (celsius * 9 / 5) + 32;
+  printf("%.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
+  return 0;
+}
