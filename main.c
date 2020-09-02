c252a8f126b58d2e31e75724b071985f69249c45
Jason Turney <jnt5211@psu.edu>
2020-09-02 19:56:00 +0000

    1

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(void) {
   char *celsius = readline("Enter temperature in celsius: ");
-  atof(celsius)
+  atof(celsius);
   fahrenheit = (celsius * 9 / 5) + 32;
   printf("%.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
   return 0;
