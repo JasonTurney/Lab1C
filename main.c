0314c286c981f36b805e8a398d58d7fe52b58745
Jason Turney <jnt5211@psu.edu>
2020-09-03 18:05:41 +0000

    4

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
+// Author: Jason Turney jnt5211@psu.edu
+// Collaborator: Sam Snavely sjs7082@psu.edu
+// Collaborator: Rasha Almutairi rea5285@psu.edu
+
 #include <stdio.h>
 #include <readline/readline.h>
+#include <stdlib.h>
 
 int main(void) 
 {
-  float celsius, fahrenheit;
-  char *celsius = readline("Enter temperature in celsius: ");
-  fahrenheit = (celsius * 9 / 5) + 32;
-  printf("%.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
+  char *temperature = readline("Enter temperature in celsius: ");
+  double celsius = atof(temperature);
+  double fahrenheit = (celsius*9/5+32);
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n",celsius, fahrenheit);
   return 0;
 }
